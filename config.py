f4a01d30a8e35bdbd6e0419f9d7bcebd1dcc7ed4
HappyZ221 <65073267+HappyZ221@users.noreply.github.com>
2020-06-06 22:48:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "NzE4ODY2NDU2MDgyNzc2MDk1.Xtva6g.he6ZoprF0x-C5pqq0YJgHmD3c9U" # токен бота
+TOKEN = "NzE4ODY2NDU2MDgyNzc2MDk1.Xtvy3A.avvA65UCqi5uIk2w-aZfKQdR68g" # токен бота
 
 POST_ID = 718880220215640124 #что то
 
